eebd01a7e18f3c8a00619f0ec11e8ec3f4faed24
Bianca-AP <72463852+Bianca-AP@users.noreply.github.com>
2020-10-31 23:33:25 -0300

    Projeto Prático - Banco de dados e PHP

---
+++ Projeto_individual_PHP_BD_Bianca/Projeto_Individual_Bianca_Alves_Pereira/index.php
@@ -0,0 +1,30 @@
+<!Doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Full Stack Eletro</title>
+        <link rel="stylesheet" href="./css/estilo.css">
+    </head>
+
+    <body>
+        <?php include_once('menu.html')?> 
+        <main>
+            <header>
+                <h1>Seja bem vindo(a)!</h1>
+            </header>    
+
+            <p> Aqui em nossa loja, <i>programadores tem desconto</i> nos produtos para sua casa!</p>
+            
+
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            
+            <br><br>
+        </main>
+
+        <footer class="rodape">
+            <p id="cor_destaque">Formas de Pagamento:</p>
+            <img width="30%" src="./imagens/formas_de_pagamento.jpg" alt="Formas de pagamento"> 
+            <p id="recodepro">&copy; Recode Pro</p>
+        </footer>
+    </body>
+</html>
---
+++ Projeto_individual_PHP_BD_Bianca/Projeto_Individual_Bianca_Alves_Pereira/lojas.php
@@ -0,0 +1,54 @@
+<!Doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Nossas lojas</title>
+        <link rel="stylesheet" href="./css/estilo.css">
+    </head>
+
+    <body>      
+        <?php include_once('menu.html')?> 
+        
+        <main>    
+            <header>
+                <h1>Nossas lojas</h1>
+            </header>
+
+            <hr>
+            <sections>             
+                <div class="endereco1">
+                    <h3>Rio de Janeiro</h3>
+                    <p>Avenida Presidente Vargas, 5000</p>
+                    <p>10 &ordm; andar</p>
+                    <p>Centro</p>
+                    <p>(21) 3333-3333</p>
+                </div>
+
+                <div class="endereco2">
+                    <h3>São Paulo</h3>
+                    <p>Avenida Paulista, 985</p>
+                    <p>3 &ordm; andar</p>
+                    <p>Jardins</p>
+                    <p>(21) 4444-4444</p>
+                </div>
+
+                <div class="endereco3">
+                    <h3>Santa Catarina</h3>
+                    <p>Rua Major Ávila, 370</p>
+                    <p>Vila Mariana</p>
+                    <p>(47) 5555-5555</p>
+                </div>
+            </sections>
+
+            <br><br><br><br>
+            
+            <br><br><br><br><br>
+        </main>
+
+        <footer class="rodape">
+            <p id="cor_destaque">Formas de Pagamento:</p>
+            <img width="30%" src="./imagens/formas_de_pagamento.jpg" alt="Formas de pagamento"> 
+            <p id="recodepro">&copy; Recode Pro</p>
+        </footer>
+    </body>
+</html>
